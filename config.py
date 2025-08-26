d6cfa3df9e5eb903c8e75ae81c08b7e5f9e076e8
ryokmt <ryokmtBot@gmail.com>
2025-08-26 15:29:28 +0900

    callbackなどの仮実装

---
+++ config.py
@@ -0,0 +1,11 @@
+# Flask
+SECRET_KEY = "test-secret" 
+
+# Supabase
+SUPABASE_URL = "https://dnwquljlapiizxcbgfgu.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRud3F1bGpsYXBpaXp4Y2JnZmd1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjEwNDM0NywiZXhwIjoyMDcxNjgwMzQ3fQ.QdOgmFAaClpD2RR-bLaT0SavDGW2jumqUBX8Txdw64M"  # サーバーサイド専用キー
+
+# LINEログイン設定
+LINE_CHANNEL_ID = "2007869022"
+LINE_CHANNEL_SECRET = "9d3a0da1004cfff70eca43f606dbf979"
+CALLBACK_URL = "https://fccapi.ddns.net/callback"
